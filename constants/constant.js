15e31e0046b8339aab47ddb6b2435ffcca0888a1
Nirav Patel <niravpatel521@gmail.com>
2022-01-20 17:54:56 +0530

    Script Update for Login and Home Page

---
+++ constants/constant.js
@@ -0,0 +1,18 @@
+export const name = "Nirav";
+export function double(n){
+    return n * n
+}
+
+export default class HomePageElement{
+    static HEADERLOGO = "#header_logo a img";
+    static LOGINBUTTON = ".login"
+
+}
+
+// export default class LoginPageElement{
+//     static headerLogo = "#header_logo a img";
+//     static loginButton = ".login"
+
+// }
+
+// export{name, double}
